e4f3f34bbf3517b48239539f92689fcbca60f02f
Ben Overmyer <ben@overmyer.net>
2020-12-31 09:36:14 -0600

    Add modifiers to dice rolls

--- src/modules/dice.js
+++ src/modules/dice.js
@@ -1,16 +1,26 @@
 const random = require("random");
 
 export function roll(expression) {
-  let split = expression.split("d");
+  let parts = expression.split("+");
+  let modifier = 0;
 
-  let number = Number(split[0]);
-  let sides = Number(split[1]);
+  if (parts.length == 1) {
+    parts = parts.split("d")
+  } else {
+    modifier = parts[1];
+    parts = parts[0].split("d");
+  }
+
+  let number = Number(parts[0]);
+  let sides = Number(parts[1]);
 
   let result = 0;
 
   for (let i = 0; i < number; i++) {
     result += random.int(1, sides);
   }
+
+  result += Number(modifier);
 
   return result;
 }
