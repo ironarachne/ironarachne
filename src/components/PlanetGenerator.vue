0c503cd895505731fef5f05059ce3670ab8f0e8a
Ben Overmyer <ben@overmyer.net>
2021-02-24 12:00:39 -0600

    Update planet generator with lighting, et al

--- src/components/PlanetGenerator.vue
+++ src/components/PlanetGenerator.vue
@@ -20,6 +20,7 @@
   import * as iarnd from "../modules/random.js";
   import * as PlanetMap from "../modules/maps/planet.js";
   import * as THREE from "three";
+  import StarfieldShader from "../modules/shaders/starfield.glsl.js";
 
   const random = require("random");
   const seedrandom = require("seedrandom");
@@ -31,11 +32,19 @@
         description: "",
         seed: "",
         renderer: {},
+        scene: {},
       };
     },
     methods: {
       generate: function () {
         random.use(seedrandom(this.seed));
+
+        if (Object.entries(this.renderer).length != 0) {
+          // If we already have a scene, destroy it and free up its resources
+          this.renderer.dispose();
+        }
+
+        let planetMap = PlanetMap.generate();
 
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 65, 800/600, 0.1, 100 );
@@ -46,16 +55,13 @@
 
         this.description = '';
 
-        let fragmentShader = PlanetMap.generateFragmentShader();
-        let vertexShader = PlanetMap.generateVertexShader();
+        let starfieldGeometry = new THREE.PlaneGeometry(50, 50, 50);
+        let starfieldMaterial = new THREE.ShaderMaterial({fragmentShader: StarfieldShader});
+        let plane = new THREE.Mesh(starfieldGeometry, starfieldMaterial);
+        this.scene.add(plane);
 
-        let spotLightColor = 0xFFFFFF;
-        let intensity = 1;
-        let spotLight = new THREE.DirectionalLight(spotLightColor, intensity);
-        spotLight.position.set(0, 10, 0);
-        spotLight.target.position.set(-5, 0, 0);
-        this.scene.add(spotLight);
-        this.scene.add(spotLight.target);
+        let fragmentShader = planetMap.baseFragmentShader;
+        let vertexShader = planetMap.vertexShader;
 
         let uniforms = THREE.UniformsUtils.merge([
           THREE.UniformsLib[ "lights" ],
@@ -63,11 +69,23 @@
 
         uniforms.u_resolution = { value: { x: 800, y: 600 } };
 
-        let geometry = new THREE.SphereGeometry(8, 32, 32);
-        let material = new THREE.ShaderMaterial({ uniforms:uniforms, fragmentShader: fragmentShader, vertexShader: vertexShader, lights: true });
-        this.planet = new THREE.Mesh( geometry, material );
-        this.planet.position.set(0, 10, 0);
-        this.scene.add(this.planet);
+        let planetGeometry = new THREE.SphereGeometry(8, 32, 32);
+
+        let planetMaterial = new THREE.ShaderMaterial({ uniforms:uniforms, fragmentShader: fragmentShader, vertexShader: vertexShader, lights: true });
+
+        let planet = new THREE.Mesh(planetGeometry, planetMaterial);
+        planet.position.set(0, 10, 0);
+
+        this.scene.add(planet);
+
+        /* TODO: finish atmosphere
+        let atmosphereShader = planetMap.atmosphereShader;
+        let planetAtmosphereGeometry = new THREE.SphereGeometry(8.1, 32, 32);
+        let atmosphereMaterial = new THREE.ShaderMaterial({ uniforms:uniforms, fragmentShader: atmosphereShader, transparent: true});
+        let atmosphere = new THREE.Mesh(planetAtmosphereGeometry, atmosphereMaterial);
+        atmosphere.position.set(0, 10, 0);
+
+        this.scene.add(atmosphere);*/
 
         this.animate();
       },
--- src/modules/maps/planet.js
+++ src/modules/maps/planet.js
@@ -1,14 +1,21 @@
 import * as ShaderTools from "../shadertools.js";
+import VertexShader from "../shaders/planet-vertex.glsl.js";
+import AtmosphereShader from "../shaders/planet-atmosphere.glsl.js";
 
 const random = require("random");
 
-export function generateVertexShader() {
-  return `varying vec2 vUv;
+export function generate() {
+  let planetMap = {};
 
-  void main() {
-    vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-  }`;
+  planetMap.vertexShader = generateVertexShader();
+  planetMap.baseFragmentShader = generateFragmentShader();
+  planetMap.atmosphereShader = AtmosphereShader;
+
+  return planetMap;
+}
+
+export function generateVertexShader() {
+  return VertexShader;
 }
 
 export function generateFragmentShader() {
@@ -21,27 +28,54 @@ export function generateFragmentShader() {
   #endif
 
   uniform vec2 u_resolution;
+
   varying vec2 vUv;
+  varying vec3 vertPos;
+  varying vec3 v_Normal;
 
   ${noiseFunction}
 
-  vec4 biome(float height, float moisture, float temperature) {
-    if (height < 0.5) {
-      return vec4(0.0, 0.0, 0.6, 1.0);
-    } else if (height < 0.6 && moisture < 0.5) {
-      return vec4(0.55, 0.55, 0.35, 1.0);
-    } else if (height < 0.8 && moisture > 0.4) {
-      return vec4(0.10, 0.35, 0.18, 1.0);
+  vec3 biome(float height, float moisture, float temperature) {
+    vec3 OCEAN = vec3(0.0, 0.0, 0.6);
+    vec3 COAST = vec3(0.0, 0.1, 0.6);
+    vec3 PLAINS = vec3(0.1, 0.45, 0.2);
+    vec3 DESERT = vec3(0.55, 0.55, 0.35);
+    vec3 MOUNTAINS = vec3(0.5, 0.5, 0.4);
+    vec3 FOREST = vec3(0.1, 0.35, 0.18);
+
+    if (height < 0.45) {
+      return OCEAN;
+    } else if (height < 0.5) {
+      return COAST;
+    } else if (height < 0.6) {
+      return mix(PLAINS, DESERT, moisture);
+    } else if (height < 0.8) {
+      return mix(PLAINS, FOREST, moisture);
     } else if (height < 0.9) {
-      return vec4(0.10, 0.45, 0.20, 1.0);
+      return mix(FOREST, MOUNTAINS, height);
     } else {
-      return vec4(0.5, 0.5, 0.4, 1.0);
+      return MOUNTAINS;
     }
   }
 
   void main(void) {
+    vec3 lightPos = vec3(-15.0, 15.0, 2.0);
+    vec3 N = normalize(v_Normal);
+    vec3 L = normalize(lightPos - vertPos);
+
     float x = vUv.x;
     float y = vUv.y;
+
+    // Lambert's cosine law
+    float lambertian = max(dot(N, L), 0.0);
+    float specular = 0.0;
+    if(lambertian > 0.0) {
+      vec3 R = reflect(-L, N);      // Reflected light vector
+      vec3 V = normalize(-vertPos); // Vector to viewer
+      // Compute the specular term
+      float specAngle = max(dot(R, V), 0.0);
+      specular = pow(specAngle, 39.0);
+    }
 
     float height = 1.0 * snoise(vec3(x * 4.0, y * 4.0, 1.0), ${seed});
     height += 0.55 * snoise(vec3(x * 8.0, y * 8.0, 1.0), ${seed});
@@ -55,9 +89,22 @@ export function generateFragmentShader() {
     float t = y/u_resolution.y;
     float temperature = abs(u_resolution.y / distance(vec2(0.0, t), vec2(0.0, u_resolution.y/2.0)));
 
-    vec4 color = biome(height, moisture, temperature);
+    vec3 biomeColor = biome(height, moisture, temperature);
 
-    gl_FragColor = color;
+    vec3 ambientColor = vec3(0.0, 0.0, 0.0);
+    vec3 specularColor = vec3(1.0, 1.0, 0.3);
+
+    float specularI = 0.12;
+    float diffuseI = 1.0;
+
+    if (height < 0.5) {
+      specularI = 0.6;
+      diffuseI = 1.1;
+    }
+
+    gl_FragColor = vec4(1.0 * ambientColor +
+      diffuseI * lambertian * biomeColor +
+      specularI * specular * specularColor, 1.0);
   }`;
 
   return shader;
