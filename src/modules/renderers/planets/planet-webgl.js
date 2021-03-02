8d0f30ef55079cc3c6221aa7542676feaa5424b6
Ben Overmyer <ben@overmyer.net>
2021-03-02 14:24:18 -0600

    Make barren planets "lumpy"

--- src/modules/renderers/planets/planet-webgl.js
+++ src/modules/renderers/planets/planet-webgl.js
@@ -295,16 +295,35 @@ export function getShaderData(classification) {
         return shader;
       },
       generateVertexShader: function () {
+        let noiseFunction = ShaderTools.simplexNoise();
+
+        let seed = random.float(0.0, 100.0);
+
         let shader = `varying vec3 vertPos;
         varying vec2 vUv;
         varying vec3 v_Normal;
+
+        ${noiseFunction}
 
         void main() {
           vUv = uv;
           vec4 vertPos4 = modelViewMatrix * vec4(position, 1.0);
           vertPos = vec3(vertPos4) / vertPos4.w;
-          gl_Position = projectionMatrix * vertPos4;
-          v_Normal = (modelViewMatrix * vec4(normal, 0.0)).xyz;
+
+          float x = vUv.x;
+          float y = vUv.y;
+
+          float height = 1.0 * snoise(vec3(x * 38.0, y * 38.0, 1.0), ${seed});
+          height += 0.55 * snoise(vec3(x * 8.0, y * 8.0, 1.0), ${seed});
+          height += 0.15 * snoise(vec3(x * 24.0, y * 24.0, 1.0), ${seed});
+          height += 0.05 * snoise(vec3(x * 48.0, y * 48.0, 1.0), ${seed});
+
+          float bumpScale = 0.25;
+
+          vec3 newPosition = position + normal * bumpScale * height;
+
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+          v_Normal = (modelViewMatrix * vec4(normal * bumpScale * height, 0.0)).xyz;
         }`;
 
         return shader;
