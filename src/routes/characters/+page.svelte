02873fe0d6ecffc40e5af33b743aace30ebda8bf
Ben Overmyer <ben@overmyer.net>
2026-05-21 09:07:58 -0230

    Move ADnD character builder to a separate Builders navigation list

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -5,12 +5,17 @@
 <section class="navigation">
   <h1>Characters &amp; People</h1>
 
+  <h2>Character Builders</h2>
+
+  <nav>
+    <a href="/fantasy/adnd/character/build">AD&amp;D 2E Character</a>
+  </nav>
+
   <h2>Character Generators</h2>
 
   <nav>
     <a href="/character">Character</a>
-    <a href="/fantasy/adnd/character">AD&amp;D 2E Character (random)</a>
-    <a href="/fantasy/adnd/character/build">AD&amp;D 2E Character Builder</a>
+    <a href="/fantasy/adnd/character">AD&amp;D 2E Character</a>
     <a href="/fantasy/dcc/character">Dungeon Crawl Classics Character</a>
     <a href="/swn/character">Stars Without Number Character</a>
     <a href="/unchartedworlds/character">Uncharted Worlds Character</a>
