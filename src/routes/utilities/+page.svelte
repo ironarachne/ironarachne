1370afea016b19933b1930a3d41b432366e7fb73
Ben Overmyer <ben@overmyer.net>
2026-05-22 12:51:25 -0230

    Remove settlement generator from Utilities page

--- src/routes/utilities/+page.svelte
+++ src/routes/utilities/+page.svelte
@@ -7,7 +7,6 @@
 
   <nav>
     <a href="/saved-data">Saved data</a>
-    <a href="/fantasy/settlement">Settlement generator</a>
     <a href="/language">Language</a>
     <a href="/species-stats">Species Height and Weight Calculator</a>
     <a href="/word-generator-cheat-sheet">Word Generator Cheat Sheet</a>
